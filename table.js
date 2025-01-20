4dbc8f196374911617cb5286a1367301c6310432
Bryan Delianov <bryan.delianov@gmail.com>
2025-01-20 14:51:06 +0100

    création du fichier table.js

---
+++ table.js
@@ -0,0 +1,13 @@
+// Boucle for pour afficher la table de multiplication de 7
+
+for(let i = 0; i <= 10; i++) {
+    console.log(i * 7);
+}
+
+// Boucle while pouyr afficher la table de multiplication de 13
+
+let i = 0;
+while (i <= 10) {
+    console.log(i * 13);
+    i++;
+}
