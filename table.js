b051cb8daf001164e2288cf85f9c00b9e25408ed
Bryan Delianov <bryan.delianov@gmail.com>
2025-01-20 15:03:42 +0100

    mise à jour du fichier table.js

--- table.js
+++ table.js
@@ -1,13 +1,13 @@
 // Boucle for pour afficher la table de multiplication de 7
 
 for(let i = 0; i <= 10; i++) {
-    console.log(i * 7);
+    console.log(i, "× 7 =", i * 7);
 }
 
 // Boucle while pouyr afficher la table de multiplication de 13
 
 let i = 0;
 while (i <= 10) {
-    console.log(i * 13);
+    console.log(i, "× 13 =", i * 13);
     i++;
 }
