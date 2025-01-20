d4bba3aa0f3768e039f8f82daa9a789fbb8f49d6
Bryan Delianov <bryan.delianov@gmail.com>
2025-01-20 15:14:32 +0100

    création de tableau.js

---
+++ tableau.js
@@ -0,0 +1,8 @@
+// Variables de type string, initialisée avec une chaîne de caractères
+let etudiant = "Bryan";
+
+// Variables de type tableau , initialisée avec une liste de chaîne de caractères
+let etudiants = ["Bryan", "Anas", "Yassine", "Badr", "Ulysse"];
+
+console.log(etudiant);
+console.log(etudiants);
